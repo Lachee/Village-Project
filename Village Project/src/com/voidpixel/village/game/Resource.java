2284d1614be31efdf88c161bef6c942ce3fca150
Lachee <lachee@lacheedomain.netii.net>
2014-09-18 15:20:17 +1000

    Made changes to resources

--- Village Project/src/com/voidpixel/village/game/Resource.java
+++ Village Project/src/com/voidpixel/village/game/Resource.java
@@ -2,5 +2,30 @@ package com.voidpixel.village.game;
 
 public class Resource {
 	public String name;
-	public String amount;
+	private int amount;
+	
+	public Resource(String name, int amount) {
+		this.name = name;
+		this.amount = amount;
+	}
+	
+	public int getAmount() { return this.amount; }
+	
+	public boolean remove(int amount) {
+		if(this.amount - amount < 0) return false;
+		this.amount -= amount;
+		return true;
+	}
+	
+	public void empty() { 
+		this.remove(this.amount);
+	}
+	
+	public void add(int amount) { this.amount += amount; }
+	public void add(Resource resource) { 
+		if(resource.name == this.name) {
+			this.add(resource.getAmount());
+			resource.empty();
+		}	
+	}
 }
